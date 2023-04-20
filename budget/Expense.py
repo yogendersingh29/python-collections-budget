befd9233764a124f81b5d052078890a6699a4793
yogendersingh29 <yogender.kushawah@sas.com>
2023-04-20 15:45:15 +0530

    commit

--- budget/Expense.py
+++ budget/Expense.py
@@ -40,3 +40,16 @@ class Expenses():
                     unnecessary_expenses.add(i)
             
             return [necessary_expenses, food_expenses, unnecessary_expenses]
+    
+    def categorize_set_comprehension(self):
+        necessary_expenses = {x for x in self.list 
+                            if x.category == 'Phone' or x.category == 'Auto and Gas' or 
+                                x.category == 'Classes'  or x.category == 'Utilities' or 
+                                x.category == 'Mortgage'}
+
+        food_expenses = {x for x in self.list 
+                            if x.category == 'Groceries' or x.category == 'Eating Out'}
+
+        unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
+        
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
